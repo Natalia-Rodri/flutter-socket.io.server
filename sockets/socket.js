097a0136f697e11ecf18b63005eb3261dab0dd77
Natalia <nataliara8695@gmail.com>
2021-07-26 20:39:20 +0200

    App terminada

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,7 +1,18 @@
 const { io } = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+
+bands.addBand( new Band( "NiziU") );
+bands.addBand( new Band( "Twice") );
+bands.addBand( new Band( "Aespa") );
+bands.addBand( new Band( "Black Pink") );
 
 io.on('connection', client => {
     console.log("Cliente conectado");
+
+    client.emit("active-bands", bands.getBands());
 
     client.on('disconnect', () => { 
         console.log("Cliente desconectado");
@@ -13,5 +24,28 @@ io.on('connection', client => {
         
     });
 
+    
+    client.on("vote-band", (payload) => {
+        bands.voteBand(payload.id);
+        io.emit("active-bands", bands.getBands());
+    });
+
+    client.on("add-band", (payload) => {
+        nuevaBanda = new Band(payload.name);
+        console.log(nuevaBanda);
+        bands.addBand(nuevaBanda);
+        io.emit("active-bands", bands.getBands());
+    });
+
+    client.on("remove-band", (payload) => {
+        bands.deleteBand(payload.id);
+        io.emit("active-bands", bands.getBands());
+    });
+
+
+    client.on("emitir-mensaje", ( payload ) => {
+        //io.emit("nuevo-mensaje", payload); //Emite a todos
+        client.broadcast.emit("nuevo-mensaje", payload); //Emite a todos menos al que lo emitió
+    })
 });
   
